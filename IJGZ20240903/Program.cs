f78430078bf377935ec0bf31689c1a071a7dc4f8
IGabriel30 <ingridgabriel30@hotmail.com>
2024-09-03 13:56:16 -0600

    Se configura el program

--- IJGZ20240903/Program.cs
+++ IJGZ20240903/Program.cs
@@ -1,12 +1,47 @@
+//importar el using para la autenticación por cookies
+using Microsoft.AspNetCore.Authentication.Cookies;
+//importar using para trabajar con JSON
+using System.Text.Json;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+//agregar servicios al contenedor de dependencias
+//agregar el servicio de controladores al contenedor
+
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+//se agrega el servicio para la exploracion de API de puntos finales
 builder.Services.AddEndpointsApiExplorer();
+//agrega servicio para la generacion de swagger
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+    .AddCookie(options =>
+    {
+        //configura el nombre del parámetro de URL para redireccionamiento no autorizado
+        options.ReturnUrlParameter = "unauthorized";
+        options.Events = new CookieAuthenticationEvents
+        {
+            OnRedirectToLogin = context =>
+            {
+                //cambia el codigo de estado No autorizado
+                context.Response.StatusCode = StatusCodes.Status401Unauthorized;
+                //establa el tipo de contenido como JSON(u otro formato)
+                context.Response.ContentType = "application/json";
+
+                var message = new
+                {
+                    error = "No Autorizado",
+                    message="Debe iniciar sesion para acceder a este recurso."
+                };
+
+                //serializa el objeto 'message' en formato
+                var jsonMessage = JsonSerializer.Serialize(message);
+                //escribe el mensaje JSON en la respuesta HTTP
+                return context.Response.WriteAsync(jsonMessage);
+            }
+        };
+    });
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -18,6 +53,7 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
