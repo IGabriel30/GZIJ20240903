35991dc28ea98e25fbb30c4449c7b83bae9355d2
IGabriel30 <ingridgabriel30@hotmail.com>
2024-09-03 16:01:13 -0600

    Modelos Matricula y NOta

---
+++ IJGZ20240903/Models/Matricula.cs
@@ -0,0 +1,10 @@
+﻿namespace IJGZ20240903.Models
+{
+    public class Matricula
+    {
+        public int Id { get; set; }
+        public string Alumno { get; set; }
+        public string Curso { get; set; }
+        public string Turno { get; set; }
+    }
+}
---
+++ IJGZ20240903/Models/Nota.cs
@@ -0,0 +1,9 @@
+﻿namespace IJGZ20240903.Models
+{
+    public class Nota
+    {
+        public int Id { get; set; }
+        public decimal Valor { get; set; }
+        public string TipoEvaluacion { get; set; }
+    }
+}
